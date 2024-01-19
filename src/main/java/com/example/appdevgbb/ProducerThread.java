7892be8a001e0b785029b82b3bc2da45dce62a96
Lynn Orrell <Lynn.Orrell@microsoft.com>
2024-01-18 19:55:10 -0600

    Add atomic counters and callback implementation to ProducerThread

--- src/main/java/com/example/appdevgbb/ProducerThread.java
+++ src/main/java/com/example/appdevgbb/ProducerThread.java
@@ -3,6 +3,8 @@ package com.example.appdevgbb;
 import java.time.Instant;
 import java.util.Properties;
 import java.util.Random;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
 
 import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.KafkaProducer;
@@ -12,11 +14,17 @@ import org.apache.kafka.clients.producer.RecordMetadata;
 import org.apache.kafka.common.serialization.StringSerializer;
 
 
-public class ProducerThread implements Runnable {
+public class ProducerThread implements Runnable, Callback {
 
     private String _topicName;
     private int _messageSizeInBytes;
     private long _sleepTimeMs;
+    
+    private long _totalRequestedSends;
+    private AtomicInteger _numRecordsSent;
+    private AtomicLong _totalRecordsSent;
+    
+    private Instant _start;
     private Properties _producerConfigProps;
     private static final int PRINT_AFTER_BATCH_SIZE = 1000;
 
@@ -25,35 +33,21 @@ public class ProducerThread implements Runnable {
         _messageSizeInBytes = messageSizeInBytes;
         _sleepTimeMs = sleepTimeMs;
         _producerConfigProps = producerConfigProps;
+        _numRecordsSent = new AtomicInteger(0);
+        _totalRecordsSent = new AtomicLong(0);
     }
 
     @Override
     public void run() {
         final Producer<String, SimpleEvent> producer = createProducer();
 
-        int numRecordsSent = 0;
-        long totalRecordsSent = 0;
-        Instant start = Instant.now();
+        _totalRequestedSends = 0;
+        _start = Instant.now();
         String message = generateRandomString(_messageSizeInBytes);
         while (true) {
             final ProducerRecord<String, SimpleEvent> record = new ProducerRecord<String, SimpleEvent>(_topicName, new SimpleEvent(message));
-            producer.send(record, new Callback() {
-                @Override
-                public void onCompletion(RecordMetadata metadata, Exception exception) {
-                    if (exception != null) {
-                        System.out.println(exception);
-                    }
-                }
-            });
-
-            numRecordsSent++;
-            totalRecordsSent++;
-            if (numRecordsSent % PRINT_AFTER_BATCH_SIZE == 0) {
-                Instant end = Instant.now();
-                System.out.println("Sent " + PRINT_AFTER_BATCH_SIZE + " records. Record size: " + _messageSizeInBytes + " bytes. Total records sent: " + totalRecordsSent + ". Records/sec: " + (numRecordsSent / (end.toEpochMilli() - start.toEpochMilli() * 1.0) * 1000));
-                start = Instant.now();
-                numRecordsSent = 0;
-            }
+            producer.send(record, this);
+            _totalRequestedSends++;
 
             if (_sleepTimeMs > 0) {
                 try {
@@ -61,6 +55,25 @@ public class ProducerThread implements Runnable {
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
+            }
+        }
+    }
+
+    @Override
+    public void onCompletion(RecordMetadata metadata, Exception exception) {
+        if (exception != null) {
+            System.out.println(exception);
+        }
+        else {
+            _numRecordsSent.incrementAndGet();
+            _totalRecordsSent.incrementAndGet();
+            final int numRecordsSent = _numRecordsSent.get();
+            final long totalRecordsSent = _totalRecordsSent.get();
+            if (numRecordsSent % PRINT_AFTER_BATCH_SIZE == 0) {
+                Instant end = Instant.now();
+                System.out.println("Record size: " + _messageSizeInBytes + " bytes. Total records sent: " + totalRecordsSent + ". Total requested sends: " + _totalRequestedSends + ". Records/sec: " + (numRecordsSent / (end.toEpochMilli() - _start.toEpochMilli() * 1.0) * 1000));
+                _start = Instant.now();
+                _numRecordsSent.set(0);
             }
         }
     }
@@ -81,5 +94,4 @@ public class ProducerThread implements Runnable {
 
         return result.toString();
     }
-
 }
