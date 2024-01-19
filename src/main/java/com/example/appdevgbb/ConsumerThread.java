ba895944da700a66126ea788bdc489c7e9a8e2bc
Lynn Orrell <Lynn.Orrell@microsoft.com>
2024-01-18 20:06:50 -0600

    Refactor ConsumerThread and remove DelegatingRebalanceListener

--- src/main/java/com/example/appdevgbb/ConsumerThread.java
+++ src/main/java/com/example/appdevgbb/ConsumerThread.java
@@ -2,10 +2,12 @@ package com.example.appdevgbb;
 
 import java.time.Duration;
 import java.time.Instant;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.Properties;
 
 import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerRebalanceListener;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
@@ -14,37 +16,27 @@ import org.apache.kafka.common.serialization.StringDeserializer;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 
-public class ConsumerThread implements Runnable {
+public class ConsumerThread implements Runnable, ConsumerRebalanceListener {
 
     private static final int PRINT_AFTER_BATCH_SIZE = 1000;
 
     private String _topicName;
     private int _numRecordsToReadBeforeCommit;
     private Properties _consumerConfigProps;
+    private final Consumer<String, SimpleEvent> _consumer;
 
     public ConsumerThread(String topicName, int numRecordsToReadBeforeCommit, Properties consumerConfigProps) {
         _topicName = topicName;
         _numRecordsToReadBeforeCommit = numRecordsToReadBeforeCommit;
         _consumerConfigProps = consumerConfigProps;
+        _consumer = createConsumer();
     }
 
     @Override
     public void run() {
-        final Consumer<String, SimpleEvent> consumer = createConsumer();
         System.out.println("Subscribing to topic: " + _topicName);
 
-        consumer.subscribe(Collections.singletonList(_topicName), new DelegatingRebalanceListener(partitions -> {
-            for(TopicPartition tp : partitions) {
-                System.out.println("Assigned Partition on Topic: " + tp.topic() + " - Partition: " + tp.partition());
-            }
-            consumer.seekToEnd(partitions);
-            return null;
-        }, partitions -> {
-            for(TopicPartition tp : partitions) {
-                System.out.println("Revoked Partition on Topic: " + tp.topic() + " - Partition: " + tp.partition());
-            }
-            return null;
-        }));
+        _consumer.subscribe(Collections.singletonList(_topicName), this);
 
         try {
             SimpleEvent simpleEvent;
@@ -55,7 +47,7 @@ public class ConsumerThread implements Runnable {
             while (true) {
                 numRecordsForCommit = 0;
 
-                final ConsumerRecords<String, SimpleEvent> records = consumer.poll(Duration.ofMillis(1000));
+                final ConsumerRecords<String, SimpleEvent> records = _consumer.poll(Duration.ofMillis(1000));
                 for (ConsumerRecord<String, SimpleEvent> consumerRecord : records) {
                     simpleEvent = consumerRecord.value();
                     if(simpleEvent != null) {
@@ -63,7 +55,7 @@ public class ConsumerThread implements Runnable {
                         numRecordsForCommit++;
                     }
                     if (_numRecordsToReadBeforeCommit > 0 && numRecordsForCommit % _numRecordsToReadBeforeCommit == 0) {
-                        consumer.commitAsync();
+                        _consumer.commitAsync();
                         numRecordsForCommit = 0;
                     }
                     totalRecordCount++;
@@ -71,16 +63,31 @@ public class ConsumerThread implements Runnable {
                         System.out.println("Total Record Count [Thread: " + Thread.currentThread().threadId() + "]: " + totalRecordCount + ". Avg end-to-end latency: " + totalEndToEndLatency / totalRecordCount + " ms.");
                     }
                 }
-                consumer.commitAsync();
+                _consumer.commitAsync();
             }
         } catch (Exception e) {
             System.out.println(e);
         } finally {
-            consumer.close();
+            _consumer.close();
         }
     }
 
     private Consumer<String, SimpleEvent> createConsumer() {
         return new KafkaConsumer<>(_consumerConfigProps, new StringDeserializer(), new JsonDeserializer<SimpleEvent>(new TypeReference<SimpleEvent>() {}));
+    }
+
+    @Override
+    public void onPartitionsRevoked(Collection<TopicPartition> partitions) {
+        for(TopicPartition tp : partitions) {
+            System.out.println("Revoked Partition on Topic: " + tp.topic() + " - Partition: " + tp.partition());
+        }
+    }
+
+    @Override
+    public void onPartitionsAssigned(Collection<TopicPartition> partitions) {
+        for(TopicPartition tp : partitions) {
+            System.out.println("Assigned Partition on Topic: " + tp.topic() + " - Partition: " + tp.partition());
+        }
+        _consumer.seekToEnd(partitions);
     }
 }
