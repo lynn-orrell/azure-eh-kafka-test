f702437c00f209e9c2701a8c46adb86b530cda64
Lynn Orrell <Lynn.Orrell@microsoft.com>
2024-01-23 14:21:09 -0600

    Remove unused import statement in Producer.java

--- src/main/java/com/example/appdevgbb/Producer.java
+++ src/main/java/com/example/appdevgbb/Producer.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 import java.util.UUID;
-import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
